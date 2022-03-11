f0ac4bc8616920321faa621f03eda989e6ee4a1a
chaosd3v <saurabhjg101@gmail.com>
2022-03-11 16:20:42 +0530

    Updated card animation with Animate.CSS

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 const card = document.querySelector(".__card");
+const ani = card.dataset.ani;
 
-loadCard();
+loadCardAnimation();
 
-function loadCard() {
-    card.classList.add("animate");
-} 
+function loadCardAnimation() {
+    card.classList.add('animated', 'infinite', ani);
+    window.setTimeout(function () {
+        card.classList.remove('animated', 'infinite', ani);
+    }, 3000);
+}
+
+card.addEventListener('mouseenter', function (e) {
+    const ani = e.target.dataset.ani;
+    e.target.classList.add('animated', 'infinite', ani);
+    window.setTimeout(function () {
+        e.target.classList.remove('animated', 'infinite', ani);
+    }, 3000);
+});
